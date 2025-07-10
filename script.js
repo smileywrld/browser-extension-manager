9c52707a06108c7b377b589b80f3fdea0be4c4a5
smileywrld <ismaileyyunusa@gmail.com>
2025-07-10 05:03:46 +0100

    feat: implement theme toggle functionality with button interaction

---
+++ script.js
@@ -0,0 +1,13 @@
+const toggleBtn = document.querySelector(".toggle-btn");
+const body = document.body;
+
+function toggleTheme() {
+	body.classList.toggle("dark");
+	if (body.classList.contains("dark")) {
+		toggleBtn.innerHTML =
+			'<img src="./assets/images/icon-sun.svg" alt="Sun Icon">';
+	} else {
+		toggleBtn.innerHTML =
+			'<img src="./assets/images/icon-moon.svg" alt="Moon Icon">';
+	}
+}
