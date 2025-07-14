8b62407e229b378b197e36b294ce3d236ca253ff
smileywrld <ismaileyyunusa@gmail.com>
2025-07-14 16:56:16 +0100

    feat: implement dynamic card generation and enhance theme toggle functionality

--- script.js
+++ script.js
@@ -1,21 +1,47 @@
-/*const toggleBtn = document.querySelector(".toggle-btn");
-const body = document.body;
+fetch("./data.json")
+	.then((response) => response.json())
+	.then((data) => {
+		const container = document.querySelector(".card-container");
 
-function toggleTheme() {
-	body.classList.toggle("dark");
-	if (body.classList.contains("dark")) {
-		toggleBtn.innerHTML =
-			'<img src="assets/images/icon-moon.svg" alt="Moon Icon">';
-	} else {
-		toggleBtn.innerHTML =
-			'<img src="assets/images/icon-sun.svg" alt="Sun Icon">';
-	}
-}
+		data.forEach((item) => {
+			const card = document.createElement("figure");
+			card.classList.add(
+				"bg-white",
+				"rounded-3xl",
+				"p-5",
+				"shadow-lg",
+				"mb-5",
+				"w-fit",
+				"h-[100%]",
+				"flex",
+				"justify-center",
+				"flex-col",
+				"card"
+			);
 
-document.addEventListener("DOMContentLoaded", toggleTheme);
+			card.innerHTML = `
+		<div class="flex gap-5 justify-around me-5">
+			<div>
+			  <img src="${item.logo}" alt="${item.title}" class="w-25">
+			</div>
+			<div class="mb-5 mx-4">
+			  <h3 class="text-2xl font-bold">${item.title}</h3>
+			  <p>${item.desc}</p>
+			</div>
+		  </div>
+		  <div class="flex justify-between">
+			<a href="#" class="bg-[#EBF4FD] px-4 rounded-full hover:bg-[#C6241A] hover:text-white remove">Remove</a>
+			<label class="relative inline-flex items-center cursor-pointer">
+			  <input type="checkbox" class="sr-only peer">
+			  <div class="w-11 h-6 bg-gray-300 rounded-full peer peer-checked:bg-[#C6241A] peer-focus:ring-2 ring-blue-500 transition-all duration-300"></div>
+			  <div class="absolute left-1 top-1 w-4 h-4 bg-white rounded-full transition-all duration-300 peer-checked:translate-x-full"></div>
+			</label>
+		  </div>
+		`;
 
-toggleBtn.addEventListener("click", toggleTheme);
-*/
+			container.appendChild(card);
+		});
+	});
 
 const toggleBtn = document.querySelector(".toggle-btn");
 const body = document.body;
