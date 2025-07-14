a46375e20f277499d0d9b899144fe0d73076f55f
smileywrld <ismaileyyunusa@gmail.com>
2025-07-14 09:58:39 +0100

    feat: implement dark mode functionality with theme toggle and style updates

--- script.js
+++ script.js
@@ -1,6 +1,40 @@
-const toggleBtn = document.querySelector(".toggle-btn");
+/*const toggleBtn = document.querySelector(".toggle-btn");
 const body = document.body;
 
-toggleBtn.addEventListener("click", () => {
+function toggleTheme() {
 	body.classList.toggle("dark");
-});
+	if (body.classList.contains("dark")) {
+		toggleBtn.innerHTML =
+			'<img src="assets/images/icon-moon.svg" alt="Moon Icon">';
+	} else {
+		toggleBtn.innerHTML =
+			'<img src="assets/images/icon-sun.svg" alt="Sun Icon">';
+	}
+}
+
+document.addEventListener("DOMContentLoaded", toggleTheme);
+
+toggleBtn.addEventListener("click", toggleTheme);
+*/
+
+const toggleBtn = document.querySelector(".toggle-btn");
+const body = document.body;
+const heading = document.querySelector(".heading");
+const card = document.querySelector(".card");
+
+function darkMode() {
+	body.classList.toggle("dark");
+	if (body.classList.contains("dark")) {
+		toggleBtn.innerHTML =
+			'<img src="assets/images/icon-moon.svg" alt="Moon Icon">';
+		heading.classList.add("text-white");
+		card.classList.add("card");
+	} else {
+		toggleBtn.innerHTML =
+			'<img src="assets/images/icon-sun.svg" alt="Sun Icon">';
+		heading.classList.remove("text-white");
+		card.classList.remove("card");
+	}
+}
+
+toggleBtn.addEventListener("click", darkMode);
